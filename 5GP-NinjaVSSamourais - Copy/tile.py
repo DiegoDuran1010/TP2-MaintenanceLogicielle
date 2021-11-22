143f5b81936dfe8d3b2a69cbff45128bd4fbbe48
Diego Felipe Duran Lezama <diegoduran2001@gmail.com>
2021-11-21 21:13:25 -0500

    Finalisation du livrable 1
    NOUS SOMMES VRAIMENT CONTENT DE NOTRE TRAVILLE !!!

--- 5GP-NinjaVSSamourais - Copy/tile.py
+++ 5GP-NinjaVSSamourais - Copy/tile.py
@@ -1,7 +1,9 @@
 from enum import Enum
 from enum import auto
+
 COULEUR_GROUND = (64, 64, 64)
 COULEUR_STONE = (96, 96, 96)
+COULEUR_WALL = (255, 255, 255)
 
 
 class TileType(Enum):
@@ -15,64 +17,63 @@ class TileType(Enum):
     SAMOURAI_START_POS_4 = auto(),
     SAMOURAI_START_POS_5 = auto(),
     SAMOURAI_START_POS_6 = auto(),
-    EXIT = auto()
+    EXIT = auto(),
+    WALKABLE = False
 
 
 class Tile:
+    TYPES_AND_COLORS = {TileType.GROUND: COULEUR_GROUND,
+                        TileType.STONE: COULEUR_STONE,
+                        TileType.WALL: COULEUR_WALL,
+                        TileType.EXIT: COULEUR_GROUND}
 
-    TYPES_AND_COLORS = {TileType.GROUND: (64, 64, 64), # !!!
-                        TileType.STONE: (96, 96, 96), # !!!
-                        TileType.WALL: (255, 255, 255), # !!!
-                        TileType.EXIT: (64, 64, 64)} # !!!
-
-    TYPES_AND_SYMBOLS = { ' ': TileType.GROUND,
+    TYPES_AND_SYMBOLS = {' ': TileType.GROUND,
                          'S': TileType.STONE,
-                         'W':TileType.WALL,
+                         'W': TileType.WALL,
                          'N': TileType.NINJA_START_POS,
-                         '1':TileType.SAMOURAI_START_POS_1,
-                         '2':TileType.SAMOURAI_START_POS_2,
+                         '1': TileType.SAMOURAI_START_POS_1,
+                         '2': TileType.SAMOURAI_START_POS_2,
                          '3': TileType.SAMOURAI_START_POS_3,
-                         '4':TileType.SAMOURAI_START_POS_4,
-                         '5':TileType.SAMOURAI_START_POS_5,
-                         '6':TileType.SAMOURAI_START_POS_6,
-                         'E':TileType.EXIT}
-
-    #print(TYPES_AND_SYMBOLS)
-
-    #TYPES_AND_SYMBOLS = {v: k for k, v in TYPES_AND_SYMBOLS.items()} # inverser le dict (demander prof que veut dire inverser)
-    #print('\n', TYPES_AND_SYMBOLS)
+                         '4': TileType.SAMOURAI_START_POS_4,
+                         '5': TileType.SAMOURAI_START_POS_5,
+                         '6': TileType.SAMOURAI_START_POS_6,
+                         'E': TileType.EXIT,
+                         TileType.WALKABLE : False}
 
     def __init__(self, walkable: bool = True, tile_type: TileType = TileType.GROUND) -> None:
         self.__walkable = walkable
         self.__tile_type = tile_type
 
     def __str__(self) -> str:
-        return self.TYPES_AND_SYMBOLS[self.__tile_type]
+        for value in self.TYPES_AND_SYMBOLS:
+            if self.TYPES_AND_SYMBOLS[value] == self.__tile_type:
+                return value
 
     @staticmethod
     def create_from_symbol(symbol: str):
 
         """Crée et configure une tuile en fonction du symbole."""
-        if symbol == Tile.TYPES_AND_SYMBOLS["S"]:
-            return Tile(tile_type=TileType.STONE, walkable=False)
-        elif symbol == Tile.TYPES_AND_SYMBOLS["W"]:
-            return Tile(tile_type=TileType.WALL, walkable=False)
-        elif symbol == Tile.TYPES_AND_SYMBOLS["N"]:
+        if symbol == "S":
+            return Tile(tile_type=TileType.STONE, walkable=Tile.TYPES_AND_SYMBOLS[TileType.WALKABLE])
+        elif symbol == "W":
+            return Tile(tile_type=TileType.WALL, walkable=Tile.TYPES_AND_SYMBOLS[TileType.WALKABLE])
+        elif symbol == "N":
             return Tile(tile_type=TileType.NINJA_START_POS)
-        elif symbol == Tile.TYPES_AND_SYMBOLS["1"]:
+        elif symbol == "1":
             return Tile(tile_type=TileType.SAMOURAI_START_POS_1)
-        elif symbol == Tile.TYPES_AND_SYMBOLS["2"]:
+        elif symbol == "2":
             return Tile(tile_type=TileType.SAMOURAI_START_POS_2)
-        elif symbol == Tile.TYPES_AND_SYMBOLS["3"]:
+        elif symbol == "3":
             return Tile(tile_type=TileType.SAMOURAI_START_POS_3)
-        elif symbol == Tile.TYPES_AND_SYMBOLS["4"]:
+        elif symbol == "4":
             return Tile(tile_type=TileType.SAMOURAI_START_POS_4)
-        elif symbol == Tile.TYPES_AND_SYMBOLS["5"]:
+        elif symbol == "5":
             return Tile(tile_type=TileType.SAMOURAI_START_POS_5)
-        elif symbol == Tile.TYPES_AND_SYMBOLS["6"]:
+        elif symbol == "6":
             return Tile(tile_type=TileType.SAMOURAI_START_POS_6)
-        elif symbol == Tile.TYPES_AND_SYMBOLS["E"]:
+        elif symbol == "E":
             return Tile(tile_type=TileType.EXIT)
+
         else:
             return Tile()
 
